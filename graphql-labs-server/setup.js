21510b5db9f2e63371c1187710f7beadc99017e4
John Paxton <johnpaxton@gmail.com>
2023-05-09 10:43:51 -0400

    Now backs up before copying

--- graphql-labs-server/setup.js
+++ graphql-labs-server/setup.js
@@ -2,6 +2,13 @@ import process from 'node:process';
 import path from 'node:path';
 import fs from 'fs-extra';
 
+let files = [
+	'codegen.ts',
+	'graphql.schema.json',
+	'schema.graphql',
+	'src',
+];
+
 let labNumber = process.argv[2];
 
 if (!labNumber) {
@@ -14,5 +21,15 @@ if (labNumber.length === 1) labNumber = '0' + labNumber;
 let labFolder = `lab-${labNumber}`;
 let labPath = path.resolve('../labs', 'starters', labFolder);
 let mainPath = path.resolve('./');
+
+// Back everything up
+let backupDest = `./tmp/backup-${Date.now()}`;
+fs.ensureDirSync(path.resolve(backupDest));
+
+for (let file of files) {
+	let src = path.resolve(`./${file}`);
+	let dest = path.resolve(`${backupDest}/${file}`);
+	fs.copySync(src, dest);
+}
 
 fs.copySync(labPath, mainPath);
